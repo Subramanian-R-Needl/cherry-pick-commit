ea37edddac0f402a1385e3c8f6e3fc9615afc273
subramanianr <subramanian.r@needl.ai>
2023-01-09 17:17:17 +0530

    update changed files only

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,6 @@ git_cmd git fetch --all
 git_cmd git checkout "${INPUT_PR_BRANCH}"
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 # git_cmd git cherry-pick "${GITHUB_SHA}"
-git_cmd checkout "${PR_BRANCH}" ${GIT_CHANGED_FILES}
+git_cmd checkout origin/"${INPUT_PR_BRANCH}" ${GIT_CHANGED_FILES}
 git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO PR: ${PR_TITLE}\""
