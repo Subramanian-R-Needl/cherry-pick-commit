9f88f946d98da14c136032201000e50349321663
subramanianr <subramanian.r@needl.ai>
2023-01-09 14:54:36 +0530

    git cmd edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,8 +37,8 @@ fi
 PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
 
 git_cmd git remote update
-git_cmd git fetch --all
 git_cmd git checkout "${INPUT_PR_BRANCH}"
+git_cmd git fetch --all
 git_cmd git pull
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git cherry-pick "${GITHUB_SHA}"
