2791b9388e7d3eec39d0e9e5e5a5da4cd4b02763
subramanianr <subramanian.r@needl.ai>
2023-01-17 10:46:48 +0530

    pr head change

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,7 @@ git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "AUTO PR FOR: \"${GIT_CHANGED_BRANCH}\""
 # git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git checkout "${INPUT_PR_BRANCH}"
+git_cmd git pull
 # git_cmd git cherry-pick "${GITHUB_SHA}"
 # echo $GIT_CHANGED_BRANCH
 # echo $GIT_CHANGED_FILES
@@ -56,5 +57,5 @@ git_cmd git checkout "${INPUT_PR_BRANCH}"
 # git_cmd git merge $GIT_CHANGED_BRANCH --allow-unrelated-histories
 # git_cmd git add -u
 # git_cmd git commit -am "Updated files" --allow-empty
-git_cmd git push -u origin "${PR_BRANCH}" -f
+git_cmd git push origin "${PR_BRANCH}" -f
 # git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "AUTO PR FOR: \"${GIT_CHANGED_BRANCH}\""
